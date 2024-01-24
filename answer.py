ef566769d5c6ca75cbc25b55a04bc7ce5b9899c1
sanjana-guptaa <guptasanjana166@gmail.com>
2024-01-24 03:08:29 -0500

    Completed the required logic as per the questions mentioned in the answer.py file

--- answer.py
+++ answer.py
@@ -1,17 +1,14 @@
 def hello_world():
-    # Write a doc string below. Doc strings can be used to provide the user with a
-    # description of what the function does.
+    """
+       This function prints "Hello, World!"
     """
 
-    """
-
-    # Write a comment; comments are very useful to other programmers.
-
-    # Write a Python statement to print Hello, World!
+    # Below is a print statement to display the necessary output
+    print("Hello, World!")
 
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    output = a + b
 
     return output
